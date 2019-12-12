2feb8b4f86859dcd046d88f1b52197e95079ee8a
Roman Piatyntsev <r.piatyntsev@gmail.com>
2019-12-12 15:59:17 +0200

    add repo

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -22,8 +22,9 @@ HTML_REPO=$(get_config_value 'html_repo' "#")
 
 
 # Make a database, if we don't already have one
-echo -e " * Creating database user '${DB_USER}'"
+mysql -u root --password=root -e "FLUSH PRIVILEGES";
 mysql -u root --password=root -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';";
+
 echo -e " * Creating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 echo -e " * Granting the wp user priviledges to the '${DB_NAME}' database"
